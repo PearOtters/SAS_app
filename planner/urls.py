10169a7b4541ea58f4fabc900ddb221637ab8d7b
PearOtters <pierrealexis.outters@gmail.com>
2025-10-25 15:42:23 +0100

    fixed index.html and mapped urls

--- planner/urls.py
+++ planner/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('events/<slug:event_slug>/', views.view_event, name="view_event"),
     path('venue/<slug:venue_slug>/', views.view_venue, name="view_venue"),
+    path('register/', views.register, name="register"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
 ]
